03d391536a43ad4cfe1973febdc046e3cf0179e4
Steven Crangle <stecra01@e119776-lin.manchester.arm.com>
2019-09-25 19:14:25 +0100

    Start of query function

--- prometheus.go
+++ prometheus.go
@@ -3,22 +3,38 @@ package main
 import (
 	"fmt"
 	"net/url"
+	"net/http"
 )
+
+
 
 type prometheusScraper struct {
 	serverAddress string
+	queryEndpoint string
 }
 
 func newPrometheusScraper(serverAddress string, controlChannel chan <- string, outputChannel <- chan int) *prometheusScraper {
 	
 	fmt.Printf("Server: %s\n", serverAddress)
 	
-	prometheusScraper := prometheusScraper {serverAddress}
+	prometheusScraper := prometheusScraper { serverAddress, "/api/v1/query_range?query=" }
+
+	queryPrometheus();
 
 	return &prometheusScraper
 }
 
-func (collector *prometheusScraper) queryProme
+func (collector *prometheusScraper) queryPrometheus() {
+	resp,err := http.get(serverAddress + queryEndpoint + 'stddev_over_time(pf_current_entries_total%7Binstance%3D~"sovapn%5B1%7C2%5D%3A9116"%7D%5B12h%5D)%20&start=1568722200&end=1569327600&step=600'); 
+
+	if err != nil {
+	// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Printf("%v\n", body)
+}
 
 func (collector *prometheusScraper) Test() {
 
