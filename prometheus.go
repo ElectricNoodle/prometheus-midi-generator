608fa09421ac6773a60275e2c16b2ea3e6686e02
Steven Crangle <stecra01@e119776-lin.manchester.arm.com>
2019-10-08 13:43:46 +0100

    Added ringbuffer

--- prometheus.go
+++ prometheus.go
@@ -1,21 +1,22 @@
-package main 
+package main
 
 import (
-    "fmt"
- //   "time"
-	"strconv"
-	"net/http"
 	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/golang-collections/go-datastructures/queue"
 )
 
 type Metric struct {
 	Instance string
-	Job string
+	Job      string
 }
 
 type Point struct {
-    Timestamp int64
-    Value float64
+	Timestamp int64
+	Value     float64
 }
 
 type TimeSeries struct {
@@ -25,155 +26,156 @@ type TimeSeries struct {
 
 type PrometheusData struct {
 	ResultType string
-    Result []TimeSeries
+	Result     []TimeSeries
 }
 
-type APIResponse  struct {
-	Status string 
-	Data PrometheusData
+type APIResponse struct {
+	Status string
+	Data   PrometheusData
 }
 
 type prometheusScraper struct {
-    Target string
-    output chan <- int
-    control <- chan ControlMessage
-    data []Point
-    mode OutputType
+	Target  string
+	output  chan<- int
+	control <-chan ControlMessage
+	mode    OutputType
+	data    *queue.RingBuffer
 }
+
+const RING_SIZE = 100
 
 type MessageType int
 
-const(
-    StartOutput MessageType = 0
-    StopOutput  MessageType = 1
-
+const (
+	StartOutput MessageType = 0
+	StopOutput  MessageType = 1
 )
 
 type OutputType int
 
-const(
-    Playback    OutputType = 0
-    Live        OutputType = 1
-    Init        OutputType = -1
+const (
+	Playback OutputType = 0
+	Live     OutputType = 1
+	Init     OutputType = -1
 )
 
 type QueryInfo struct {
-    Query string
-    Start float64
-    End   float64
-    Step  int
+	Query string
+	Start float64
+	End   float64
+	Step  int
 }
 
 type ControlMessage struct {
-    Type MessageType
-    OutputType OutputType
-    QueryInfo QueryInfo 
+	Type       MessageType
+	OutputType OutputType
+	QueryInfo  QueryInfo
 }
-
 
 func (tp *Point) UnmarshalJSON(data []byte) error {
 
-    var v []interface{}
-    
+	var v []interface{}
+
 	if err := json.Unmarshal(data, &v); err != nil {
 		fmt.Printf("Error while decoding Point %v\n", err)
 		return err
-    }
-    
+	}
+
 	tp.Timestamp = int64(v[0].(float64))
 	tp.Value, _ = strconv.ParseFloat(v[1].(string), 64)
 
 	return nil
-} 
+}
 
-func newPrometheusScraper(queryEndpoint string, mode string, controlChannel <- chan ControlMessage, outputChannel chan <- int) *prometheusScraper {
-	
-	prometheusScraper := prometheusScraper { queryEndpoint,outputChannel, controlChannel, []Point{}, Init }
-    go prometheusScraper.controlThread()
-	
+func newPrometheusScraper(queryEndpoint string, mode string, controlChannel <-chan ControlMessage, outputChannel chan<- int) *prometheusScraper {
+
+	prometheusScraper := prometheusScraper{queryEndpoint, outputChannel, controlChannel, Init, queue.NewRingBuffer(RING_SIZE)}
+	go prometheusScraper.controlThread()
+
 	return &prometheusScraper
 }
 
 /* This function listens for any incoming messages and handles them accordingly */
 func (collector *prometheusScraper) controlThread() {
-    for {
+	for {
 
-        message := <-collector.control
+		message := <-collector.control
 
-        switch message.Type {
-        
-        case StartOutput:
-           
-            fmt.Printf("Starting output thread.. Playback Type: %i\n", message.OutputType)
-            fmt.Printf("Query: %s Start: %i Stop: %i Step: %i \n", message.QueryInfo.Query, message.QueryInfo.Start, message.QueryInfo.End, message.QueryInfo.Step)
-            collector.queryPrometheus(message.OutputType, message.QueryInfo.Query, message.QueryInfo.Start, message.QueryInfo.End, message.QueryInfo.Step)
+		switch message.Type {
 
-        case StopOutput:
-            fmt.Printf("Stopping output thread..\n")
-        default:
-            fmt.Printf("Unknown MessageType: %i \n", message.Type )
-        } 
-    }
+		case StartOutput:
+
+			fmt.Printf("Starting output thread.. Playback Type: %d\n", message.OutputType)
+			fmt.Printf("Query: %s Start: %f Stop: %f Step: %d \n", message.QueryInfo.Query, message.QueryInfo.Start, message.QueryInfo.End, message.QueryInfo.Step)
+			collector.queryPrometheus(message.OutputType, message.QueryInfo.Query, message.QueryInfo.Start, message.QueryInfo.End, message.QueryInfo.Step)
+
+		case StopOutput:
+			fmt.Printf("Stopping output thread..\n")
+		default:
+			fmt.Printf("Unknown MessageType: %d \n", message.Type)
+		}
+	}
 }
 
 /*  Stores the initial time series data, starts the output thread, and also the live playback query thread if required. */
 func (collector *prometheusScraper) queryPrometheus(mode OutputType, query string, start float64, end float64, step int) bool {
 
-    collector.data = collector.getTimeSeriesData(query, start, end, step)
+	// /collector.data = collector.getTimeSeriesData(query, start, end, step)
 
-    if mode == Live {
-        go collector.queryThread(query, step)
-    }
+	if mode == Live {
+		go collector.queryThread(query, step)
+	}
 
-    go collector.outputThread()
+	go collector.outputThread()
 
-    return true
+	return true
 }
 
 func (collector *prometheusScraper) outputThread() {
-    /* Query data structure using mutex in timed loop based on step and emit message to output channel. */
+	/* Query data structure using mutex in timed loop based on step and emit message to output channel. */
+
 }
 
 func (collector *prometheusScraper) queryThread(query string, step int) {
-    /* Populate data structure using mutex in timed loop based on step. Need to make sure the query poll rate is a division of step. */
+	/* Populate data structure using mutex in timed loop based on step. Need to make sure the query poll rate is a division of step. */
+
 }
- 
+
 /* Returns an array of points which represent the timeseries data for the specified query.
    NOTE: Doesn't handle more than one set of time series (Result[0]), Will expand to handle it later.
 */
 func (collector *prometheusScraper) getTimeSeriesData(query string, start float64, end float64, step int) []Point {
 	request, err := http.NewRequest("GET", collector.Target, nil)
-    
-    if err != nil {
-        fmt.Printf("%s\n", err)
-        return []Point{}
-    }
 
-    
-    q := request.URL.Query()
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return []Point{}
+	}
 
-    q.Add("query", query)
-    q.Add("start", strconv.FormatFloat(start, 'f', 6, 64))
-    q.Add("end",   strconv.FormatFloat(end, 'f', 6, 64))
-    q.Add("step",  strconv.Itoa(step))
+	q := request.URL.Query()
 
-    request.URL.RawQuery = q.Encode()
+	q.Add("query", query)
+	q.Add("start", strconv.FormatFloat(start, 'f', 6, 64))
+	q.Add("end", strconv.FormatFloat(end, 'f', 6, 64))
+	q.Add("step", strconv.Itoa(step))
 
-    fmt.Printf("URL      %+v\n", request.URL)
-    fmt.Printf("RawQuery %+v\n", request.URL.RawQuery)
-    fmt.Printf("Query    %+v\n", request.URL.Query())
+	request.URL.RawQuery = q.Encode()
 
-    result,err := http.DefaultClient.Do(request)
-    defer result.Body.Close()
+	fmt.Printf("URL      %+v\n", request.URL)
+	fmt.Printf("RawQuery %+v\n", request.URL.RawQuery)
+	fmt.Printf("Query    %+v\n", request.URL.Query())
 
-    var apiResponse APIResponse
+	result, err := http.DefaultClient.Do(request)
+	defer result.Body.Close()
 
-    e := json.NewDecoder(result.Body).Decode(&apiResponse)
+	var apiResponse APIResponse
 
-    if e != nil {
-        fmt.Printf("%s\n", e)
-        return []Point{}
-    }
-    /* Need to check that return value is valid before returning. */
-    return apiResponse.Data.Result[0].Values
+	e := json.NewDecoder(result.Body).Decode(&apiResponse)
+
+	if e != nil {
+		fmt.Printf("%s\n", e)
+		return []Point{}
+	}
+	/* Need to check that return value is valid before returning. */
+	return apiResponse.Data.Result[0].Values
 }
